ea96acc63080002372621d81a63c6a5c48390559
Dmitry <75312665+WOLFERSER@users.noreply.github.com>
2022-09-26 19:27:10 +0300

    Add files via upload

---
+++ WolfHub.lua
@@ -0,0 +1,38 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "WolfHub", HidePremium = false, SaveConfig = true, ConfigFolder = "ConfigWolfHub", IntroText = "Wolf Hub"})
+
+local Credits = Window:MakeTab({
+	Name = "Credits",
+	Icon = "rbxassetid://136931266",
+	PremiumOnly = false
+})
+
+local BloxFruits = Window:MakeTab({
+	Name = "Blox Fruits",
+	Icon = "rbxassetid://136931266",
+	PremiumOnly = false
+})
+
+Credits:AddButton({
+	Name = "Discord Link: bit.ly/dswolfhub",
+	Callback = function()
+    print('Discord Link: bit.ly/dswolfhub')
+  	end    
+})
+
+Credits:AddButton({
+	Name = "Owner of hub: WOLF#3030",
+	Callback = function()
+    print('Owner of hub: WOLF#3030')
+  	end    
+})
+
+BloxFruits:AddButton({
+	Name = "Orange Cheat",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+  	end    
+})
+
+OrionLib:Init()
